cc59f357e7a673b2c3b9d756876a83c36785bf9f
rt656arch <rt656@drexel.edu>
2025-12-04 07:44:22 -0500

    author profile

--- final/build/postload.js
+++ final/build/postload.js
@@ -1,6 +1,3 @@
-
-
-
 const likeBtn = document.getElementById("like-btn");
 const likeCount = document.getElementById("like-count");
 
@@ -11,6 +8,8 @@ likeBtn.addEventListener("click", () => {
     if (!liked) {
         count++;
         liked = true;
+
+        likeBtn.style.color = "#053EFF";
 
         // tilt up
         likeBtn.style.transform = "rotate(-30deg)";
@@ -24,12 +23,75 @@ likeBtn.addEventListener("click", () => {
         count--;
         liked = false;
 
-        likeBtn.style.transform = "rotate(30deg)";
-
-        setTimeout(() => {
-            likeBtn.style.transform = "rotate(0deg)";
-        }, 250);
+        likeBtn.style.color = "#AAAAAA";
+        likeBtn.style.transform = "rotate(0deg)";
     }
 
     likeCount.textContent = count;
+});
+
+const likeBtn1 = document.getElementById("like-btn1");
+const likeCount1 = document.getElementById("like-count1");
+
+let liked1 = false;
+let count1 = 0;
+
+likeBtn1.addEventListener("click", () => {
+    if (!liked1) {
+        count1++;
+        liked1 = true;
+
+        likeBtn1.style.color = "#053EFF";
+
+        // tilt up
+        likeBtn1.style.transform = "rotate(-30deg)";
+
+        // return to normal after animation
+        setTimeout(() => {
+            likeBtn1.style.transform = "rotate(0deg)";
+        }, 250);
+
+    } else {
+        count1--;
+        liked1 = false;
+
+        likeBtn1.style.color = "#AAAAAA";
+        likeBtn1.style.transform = "rotate(0deg)";
+    }
+
+    likeCount1.textContent = count1;
+});
+
+
+const authorName = document.querySelector('.product-author');
+const popup = document.querySelector('.author-profile-popup');
+let isHovering = false;
+
+function showPopup() {
+    popup.classList.add('visible');
+}
+
+function hidePopup() {
+    popup.classList.remove('visible');
+}
+
+authorName.addEventListener('mouseenter', () => {
+    isHovering = true;
+    showPopup();
+});
+
+authorName.addEventListener('mouseleave', () => {
+    setTimeout(() => {
+        if (!isHovering) hidePopup();
+    }, 50);
+});
+
+popup.addEventListener('mouseenter', () => {
+    isHovering = true;
+    showPopup();
+});
+
+popup.addEventListener('mouseleave', () => {
+    isHovering = false;
+    hidePopup();
 });
